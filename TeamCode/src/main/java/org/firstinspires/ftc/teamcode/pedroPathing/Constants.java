1a211bbed4f0cb57dc94b871acf828ca382a9daf
ClaireO-Brien <claireobrienca@gmail.com>
2025-11-21 17:12:45 -0800

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -19,9 +19,9 @@ public class Constants {
             .mass(12.60987)
             .forwardZeroPowerAcceleration(-53.7961591924281)
             .lateralZeroPowerAcceleration(-84.89915877059722)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.1, 0, 0, 0))
-            .headingPIDFCoefficients(new PIDFCoefficients(0.85, 0, 0, 0))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.006,0.0,0,0.6,0.0))
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.07, 0, 0.002, 0.02))
+            .headingPIDFCoefficients(new PIDFCoefficients(1.0, 0, 0.02, 0.01))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.05,0.0,0,0.6,0.01))
             .centripetalScaling(0.001);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
@@ -58,7 +58,7 @@ public class Constants {
     public static PathConstraints pathConstraints = new PathConstraints(
             0.99,
             100,
-            2,
+            1,
             1);
 
 
