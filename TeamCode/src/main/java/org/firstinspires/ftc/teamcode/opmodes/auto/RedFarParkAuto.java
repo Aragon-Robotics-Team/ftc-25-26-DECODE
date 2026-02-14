e183b9228af729782dc10ec0143561584ed8ee3c
braydenwaffles <hobrayden@gmail.com>
2026-02-14 14:52:17 -0800

    redfarparkauto
    ts useless

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RedFarParkAuto.java
@@ -0,0 +1,283 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import static org.firstinspires.ftc.teamcode.RobotConstants.BallColors.GREEN;
+import static org.firstinspires.ftc.teamcode.RobotConstants.BallColors.PURPLE;
+
+import android.annotation.SuppressLint;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
+import com.seattlesolvers.solverslib.command.ParallelDeadlineGroup;
+import com.seattlesolvers.solverslib.command.ParallelRaceGroup;
+import com.seattlesolvers.solverslib.command.RunCommand;
+import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
+import com.seattlesolvers.solverslib.command.WaitCommand;
+import com.seattlesolvers.solverslib.command.WaitUntilCommand;
+import com.seattlesolvers.solverslib.pedroCommand.FollowPathCommand;
+
+import org.firstinspires.ftc.teamcode.AutoPoseSaver;
+import org.firstinspires.ftc.teamcode.RobotConstants;
+import org.firstinspires.ftc.teamcode.commands.DeferredCommand;
+import org.firstinspires.ftc.teamcode.commands.MoveSpindexerAndUpdateArrayCommand;
+import org.firstinspires.ftc.teamcode.commands.ShootSortedBallsCommandSequence;
+import org.firstinspires.ftc.teamcode.commands.WaitForColorCommand;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.subsystems.ColorSensorsSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.GateSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.LEDSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.LimelightSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.SpindexerSubsystem;
+
+import java.util.Arrays;
+@Configurable
+@Autonomous(name = "\uD83D\uDD34 Far Park", group = "angryBirds", preselectTeleOp = "RedTeleOp")
+public class RedFarParkAuto extends CommandOpMode {
+    //3 sorted preload, 6 sorted spike mark, gate intake
+    public static class Paths {
+        public PathChain shootFarPreload;
+        public PathChain parkFar;
+        public static class Poses {
+            public static final Pose LAUNCH = new Pose(89.4,18.5, Math.toRadians(63));
+            public static final Pose START = new Pose(102.5, 8, Math.toRadians(90));
+        }
+
+        public Paths(Follower follower) {
+            shootFarPreload = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierLine(Poses.START, Poses.LAUNCH)
+                    )
+                    .setLinearHeadingInterpolation(Poses.START.getHeading(), Poses.LAUNCH.getHeading())
+                    .build();
+
+            parkFar = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierLine(Poses.LAUNCH, new Pose(110, 8))
+                    )
+                    .setTangentHeadingInterpolation()
+                    .build();
+        }
+    }
+
+    //Since intakeartifacts is called at very different times (called when on the gate, called before driving to the row of balls)
+    //we might need to split it up.
+    private SequentialCommandGroup intakeArtifacts() {
+        return new SequentialCommandGroup(
+                new InstantCommand(() -> intake.set(IntakeSubsystem.IntakeState.INTAKEIN_ROLLERSIN)),
+                new WaitForColorCommand(colorsensor).withTimeout(3000),
+                new MoveSpindexerAndUpdateArrayCommand(spindexer, gate, 1, true, false),
+                new WaitCommand(100),
+                new WaitForColorCommand(colorsensor).withTimeout(500),
+                new WaitCommand(100),
+                new MoveSpindexerAndUpdateArrayCommand(spindexer, gate, 1, true, false),
+                new WaitForColorCommand(colorsensor).withTimeout(500)
+        );
+    }
+    private SequentialCommandGroup shootFourTimesWithDelay() {
+        return new SequentialCommandGroup(
+                new MoveSpindexerAndUpdateArrayCommand(spindexer, gate, 1, true, false),
+                new WaitCommand(100),
+                new MoveSpindexerAndUpdateArrayCommand(spindexer, gate, 1, true, false),
+                new WaitCommand(300),
+                new MoveSpindexerAndUpdateArrayCommand(spindexer, gate, 1, true, false),
+                new WaitCommand(100),
+                new MoveSpindexerAndUpdateArrayCommand(spindexer, gate, 1, true, false),
+                new WaitCommand(100)
+        );
+    }
+    public Pose currentPose;
+    public RobotConstants.BallColors[] motif = new RobotConstants.BallColors[]{PURPLE,PURPLE,PURPLE};
+
+    //voltage compensation
+    public VoltageSensor voltageSensor;
+    double currentVoltage = 14;
+    private boolean slowMode = false;
+    public ElapsedTime lastVoltageCheck = new ElapsedTime();
+    private ElapsedTime timer;
+    private Follower follower;
+
+    //update starting pose
+    public static Pose startingPose = Paths.Poses.START;
+    private IntakeSubsystem intake;
+    private ShooterSubsystem shooter;
+    private SpindexerSubsystem spindexer;
+    private ColorSensorsSubsystem colorsensor;
+    private GateSubsystem gate;
+    private LEDSubsystem led;
+    private LimelightSubsystem limelight;
+
+    //debugging
+    int debug = 0;
+    InstantCommand setCount(int n) {
+        return new InstantCommand(() -> debug = n);
+    }
+    void scanMotif() {
+        limelight.takeSnapshot("MOTIF");
+        Object motifid = limelight.detectMotif(limelight.getResult());
+        if (motifid != null) {
+            switch ((int) motifid) {
+                case 21:
+                    motif = new RobotConstants.BallColors[]{GREEN, PURPLE, PURPLE}; //changed to actual motif
+                    break;
+                case 22:
+                    motif = new RobotConstants.BallColors[]{PURPLE, GREEN, PURPLE};
+                    break;
+                case 23:
+                    motif = new RobotConstants.BallColors[]{PURPLE, PURPLE, GREEN}; //changed to actual motif
+                    break;
+            }
+        }
+    }
+    SequentialCommandGroup pulseIntakeOut() {
+        return new SequentialCommandGroup(
+                new InstantCommand(() -> intake.set(IntakeSubsystem.IntakeState.INTAKEOUT_ROLLERSIN)),
+                new WaitCommand(600),
+                new InstantCommand(() -> intake.set(IntakeSubsystem.IntakeState.INTAKEIN_ROLLERSIN))
+        );
+    }
+    @Override
+    public void initialize() {
+        timer = new ElapsedTime();
+        timer.reset();
+
+        //systems and pedro
+        follower = Constants.createFollower(hardwareMap);
+        follower.setPose(startingPose);
+        follower.setMaxPower(1.0);
+        intake = new IntakeSubsystem(hardwareMap);
+        intake.set(IntakeSubsystem.IntakeState.INTAKESTILL_ROLLERSSTILL);
+        shooter = new ShooterSubsystem(hardwareMap);
+        spindexer = new SpindexerSubsystem(hardwareMap);
+        colorsensor = new ColorSensorsSubsystem(hardwareMap);
+        gate = new GateSubsystem(hardwareMap);
+        gate.down();
+        led = new LEDSubsystem(hardwareMap);
+        voltageSensor = hardwareMap.get(VoltageSensor.class, "Control Hub");
+        limelight = new LimelightSubsystem(hardwareMap);
+        limelight.setPipeline(LimelightSubsystem.LIMELIGHT_PIPELINES.APRILTAG);
+        colorsensor.updateSensor1();
+        colorsensor.updateSensor2();
+        colorsensor.updateBack();
+        lastVoltageCheck.reset();
+        Paths paths = new Paths(follower);
+
+
+        // DO NOT REMOVE! Resetting FTCLib Command Scheduler
+        //Idk what this is but I think it's important it was from the github code
+        super.reset();
+
+        // Initialize subsystems
+        register(intake, spindexer, shooter, colorsensor, led, gate);
+        spindexer.set(115);
+        SequentialCommandGroup far_volume = new SequentialCommandGroup(
+                new InstantCommand(() -> { //setup
+                    shooter.setTargetTicks(1450);
+                    gate.down();
+                    gate.down();
+                }),
+                new WaitCommand(1),
+                new InstantCommand(() -> gate.down()), //bruh
+                //Preload
+                new ParallelDeadlineGroup(
+                        new FollowPathCommand(follower, paths.shootFarPreload, true)
+                                .alongWith(new WaitUntilCommand(() -> follower.getPathCompletion() > 0.1).andThen(new InstantCommand(() -> intake.set(IntakeSubsystem.IntakeState.INTAKEIN_ROLLERSIN))))
+                ),
+                setCount(1),
+                new WaitUntilCommand(() -> shooter.isAtTargetVelocity()),
+                new WaitCommand(500),
+                setCount(2),
+                shootFourTimesWithDelay(),
+                setCount(3),
+
+                //move to end pos
+                new InstantCommand(() -> follower.setMaxPower(1.0)),
+                new FollowPathCommand(follower, paths.parkFar)
+        );
+
+        schedule(
+                new RunCommand(() -> follower.update())
+        );
+        schedule(new SequentialCommandGroup(
+                far_volume
+        ));
+
+
+    }
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void run() {
+        colorsensor.updateSensor1();
+        colorsensor.updateSensor2();
+        colorsensor.updateBack();
+        if ((Math.abs(spindexer.getCurrentPosition() - spindexer.getPIDSetpoint()) < 60)) {
+            spindexer.handleUpdateArray(colorsensor.getIntakeSensor1Result(), colorsensor.getIntakeSensor2Result(), colorsensor.getBackResult());
+        }
+        if (shooter.getVelocityTicks() - shooter.getTargetTicks() < -30) {
+            led.setColor(LEDSubsystem.LEDState.RED);
+        }
+        else if (shooter.getVelocityTicks() - shooter.getTargetTicks() > 50) {
+            led.setColor(LEDSubsystem.LEDState.BLUE);
+        }
+        else {
+            led.setColor(LEDSubsystem.LEDState.GREEN);
+        }
+        //Voltage compensation code
+        if (lastVoltageCheck.milliseconds() > 500) { //check every 500ms
+            currentVoltage = voltageSensor.getVoltage();
+            spindexer.updatePIDVoltage(currentVoltage);
+            shooter.updatePIDVoltage(currentVoltage);
+            lastVoltageCheck.reset();
+        }
+
+
+        telemetry.addData("Loop Time", timer.milliseconds());
+
+        telemetry.addData("Debug Counter", debug);
+        telemetry.addData("Detected Motif", Arrays.toString(motif));
+
+//        telemetry.addData("spindexer output", spindexer.getOutput());
+//        telemetry.addData("spindexer setpoint", spindexer.getPIDSetpoint());
+//        telemetry.addData("spindexer pos", spindexer.getCurrentPosition());
+        telemetry.addData("spindexer's balls", Arrays.toString(spindexer.getBalls()));
+
+        telemetry.addData("------------------",null);
+
+        telemetry.addData("shooter target velocity", shooter.getTargetTicks());
+        telemetry.addData("shooter actual velocity", shooter.getVelocityTicks());
+
+        telemetry.addData("------------------",null);
+
+        telemetry.addData("current pos", String.format("X: %8.2f, Y: %8.2f", follower.getPose().getX(), follower.getPose().getY()));
+        telemetry.addData("current heading", String.format("Heading: %.4f", follower.getPose().getHeading()));
+        telemetry.addData("t value", follower.getCurrentTValue());
+        telemetry.addData("------------------",null);
+        currentPose = follower.getPose().plus(
+                new Pose(-2,0) //DO NOT MIRROR THIS! INVERT THE X AXIS *ONLY*
+        ); //Auto->teleop offset
+        AutoPoseSaver.lastPose = currentPose;
+        timer.reset();
+        telemetry.update();
+        super.run();
+
+    }
+
+    @Override
+    public void end() {
+        AutoPoseSaver.lastPose = currentPose;
+        super.end();
+    }
+}
